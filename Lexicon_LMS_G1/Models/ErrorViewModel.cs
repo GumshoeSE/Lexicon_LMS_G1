25ab824d091fdf539b51c72a0448e52764a6961c
Mikael Larsson <micke.lar@gmail.com>
2022-03-14 13:54:50 +0100

    Add project files.

---
+++ Lexicon_LMS_G1/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace Lexicon_LMS_G1.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
