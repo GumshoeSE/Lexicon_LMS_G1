f138474f7fce44304df9d56e32464b5a9d281ceb
Mikael Larsson <micke.lar@gmail.com>
2022-03-16 10:46:25 +0100

    Add TestProject

---
+++ Lexicon_LMS_G1.Tests/UnitTest1.cs
@@ -0,0 +1,13 @@
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Lexicon_LMS_G1.Tests
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
