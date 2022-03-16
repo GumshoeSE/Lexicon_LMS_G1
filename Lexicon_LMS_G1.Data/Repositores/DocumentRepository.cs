786429bcd210ef78fec8db0e946a34c0e87b810d
Mikael Larsson <micke.lar@gmail.com>
2022-03-16 16:43:08 +0100

    Add Repositorys

---
+++ Lexicon_LMS_G1.Data/Repositores/DocumentRepository.cs
@@ -0,0 +1,17 @@
+﻿using Lexicon_LMS_G1.Data.Data;
+using Lexicon_LMS_G1.Entities.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lexicon_LMS_G1.Data.Repositores
+{
+    public class DocumentRepository : BaseRepository<Document>
+    {
+        public DocumentRepository(ApplicationDbContext context) : base(context)
+        {
+        }
+    }
+}
---
+++ Lexicon_LMS_G1.Data/Repositores/IBaseRepository.cs
@@ -0,0 +1,14 @@
+﻿using System.Linq.Expressions;
+
+namespace Lexicon_LMS_G1.Data.Repositores
+{
+    public interface IBaseRepository<T> where T : class
+    {
+        bool Add(T newItem);
+        bool Delete(params object?[]? keyValues);
+        Task<IEnumerable<T>> GetAsync();
+        T? GetById(params object?[]? keyValues);
+        IEnumerable<T> GetByPredicate(Expression<Func<T, bool>> predicate);
+        bool Update(T newItem, params object?[]? keyValues);
+    }
+}
