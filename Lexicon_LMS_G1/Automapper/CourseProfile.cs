9897df6d69c3c4025ad2a8847465fa1700c95bd3
Ville Granfors <villeg@kth.se>
2022-03-18 10:45:52 +0100

    Added a method to save changes in the base repo class. And Automapper.

---
+++ Lexicon_LMS_G1/Automapper/CourseProfile.cs
@@ -0,0 +1,14 @@
+﻿using AutoMapper;
+using Lexicon_LMS_G1.Entities.Entities;
+using Lexicon_LMS_G1.Models.ViewModels;
+
+namespace Lexicon_LMS_G1.Automapper
+{
+    public class CourseProfile : Profile
+    {
+        public CourseProfile()
+        {
+            CreateMap<Course, CourseCreateViewModel>().ReverseMap();
+        }
+    }
+}
--- Lexicon_LMS_G1/Models/ViewModels/CourseCreateViewModel.cs
+++ Lexicon_LMS_G1/Models/ViewModels/CourseCreateViewModel.cs
@@ -5,6 +5,7 @@ namespace Lexicon_LMS_G1.Models.ViewModels
     public class CourseCreateViewModel
     {
         [Required]
+        [StringLength(40, ErrorMessage = "The {0} value cannot exceed {1} characters. ")]
         public string Name { get; set; } = string.Empty;
 
         [Required]
