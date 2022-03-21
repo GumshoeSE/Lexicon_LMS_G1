dac1818a82beb4c06700ae95dcd26c3134cdf37a
Ville Granfors <villeg@kth.se>
2022-03-21 13:01:20 +0100

    A new ViewComponent Toast to display messages

---
+++ Lexicon_LMS_G1/ViewComponents/ToastViewComponent.cs
@@ -0,0 +1,19 @@
+﻿using Lexicon_LMS_G1.Entities.ViewModels;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Lexicon_LMS_G1.ViewComponents
+{
+    public class ToastViewComponent : ViewComponent
+    {
+
+        public async Task<IViewComponentResult> InvokeAsync(string message)
+        {
+            var model = new ToastViewModel
+            {
+                Message = message
+            };
+
+            return View(model);
+        }
+    }
+}
