67919a13d45537045634f2259ed8bcedab8d24b7
Mikael Larsson <micke.lar@gmail.com>
2022-03-24 16:24:53 +0100

    User pagination and search

---
+++ Lexicon_LMS_G1.Entities/ViewModels/UsersViewModel.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lexicon_LMS_G1.Entities.ViewModels
+{
+    public class UsersViewModel
+    {
+        public string Name { get; } = "Name";
+        public string Email { get; } = "Email";
+        public string CourseId { get; } = "Course Id";
+
+
+        public int CurrentPageIndex { get; set; }
+        public int TotalCount { get; set; }
+        public int TotalPages { get; set; }
+        public int PageSize { get; set; }
+        public bool HasPrevious => CurrentPageIndex > 1;
+        public bool HasNext => CurrentPageIndex < TotalPages;
+
+
+        public IEnumerable<UserViewModel> Users { get; set; } = new List<UserViewModel>();
+    }
+}
---
+++ Lexicon_LMS_G1/Automapper/UserProfile.cs
@@ -0,0 +1,14 @@
+﻿using AutoMapper;
+using Lexicon_LMS_G1.Entities.Entities;
+using Lexicon_LMS_G1.Entities.ViewModels;
+
+namespace Lexicon_LMS_G1.Automapper
+{
+    public class UserProfile : Profile
+    {
+        public UserProfile()
+        {
+            CreateMap<ApplicationUser, UserViewModel>().ReverseMap();
+        }
+    }
+}
