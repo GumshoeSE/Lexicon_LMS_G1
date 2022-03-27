674a07e9c786966adfbcce9a19eb413b95f2054c
Cecilia Sjögren <cisci_13@hotmail.com>
2022-03-27 11:21:56 +0200

    Added Mapping for activity

---
+++ Lexicon_LMS_G1/Automapper/ActivityProfile.cs
@@ -0,0 +1,14 @@
+﻿using AutoMapper;
+using Lexicon_LMS_G1.Entities.Entities;
+using Lexicon_LMS_G1.Entities.ViewModels;
+
+namespace Lexicon_LMS_G1.Automapper
+{
+    public class ActivityProfile : Profile
+    {
+        public ActivityProfile()
+        {
+            CreateMap<Activity, ActivityTeacherViewModel>().ReverseMap();
+        }
+    }
+}
