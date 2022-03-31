40e888d3cc3599096d440baa6c773078f8130351
Cecilia Sjögren <cisci_13@hotmail.com>
2022-03-31 15:59:59 +0200

    fixed border

--- Lexicon_LMS_G1/Services/TimeLeftSevice.cs
+++ Lexicon_LMS_G1/Services/TimeLeftSevice.cs
@@ -14,7 +14,7 @@
             else if (timeleft.Days < 8)
                 return border ? "border-info" : timeleft.Days.ToString() + " days";
             else
-                return border ? "border-primary" : (timeleft.Days / 7).ToString() + " weeks";
+                return border ? "border-secondary" : (timeleft.Days / 7).ToString() + " weeks";
         }
     }
 }
