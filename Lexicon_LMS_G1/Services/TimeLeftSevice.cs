15b92763558141349e41bc2e38296e90f5f9590f
Cecilia Sjögren <cisci_13@hotmail.com>
2022-03-31 17:18:37 +0200

    Moved attendees button to top and made it into an icon

--- Lexicon_LMS_G1/Services/TimeLeftSevice.cs
+++ Lexicon_LMS_G1/Services/TimeLeftSevice.cs
@@ -7,12 +7,13 @@
             var timeleft = endDate - DateTime.Now;
             if (timeleft.TotalHours < 1)
                 return border ? "border-warning" : timeleft.Minutes.ToString() + " min";
+
             else if (timeleft.TotalDays < 1)
-            {
                 return border ? "border-warning" : timeleft.Hours.ToString() + " h";
-            }
+
             else if (timeleft.Days < 8)
                 return border ? "border-info" : timeleft.Days.ToString() + " days";
+
             else
                 return border ? "border-secondary" : (timeleft.Days / 7).ToString() + " weeks";
         }
